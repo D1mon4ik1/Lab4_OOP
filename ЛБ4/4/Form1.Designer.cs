958342f82deba8f56bd948ff1fd0906f7a823fc9
D1mon4ik1 <dima.pysarenko1@gmail.com>
2024-04-02 22:19:02 +0300

    lab4

---
+++ ЛБ4/4/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿namespace _4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            tabPage2 = new TabPage();
+            label11 = new Label();
+            label10 = new Label();
+            textBox5 = new TextBox();
+            label8 = new Label();
+            label9 = new Label();
+            textBox4 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            button2 = new Button();
+            textBox2 = new TextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(776, 426);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(label3);
+            tabPage1.Controls.Add(label2);
+            tabPage1.Controls.Add(label1);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(textBox1);
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(768, 393);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Завдання 1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(133, 239);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 4;
+            label3.Text = "label3";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(32, 239);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(3, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(330, 152);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(32, 74);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(694, 54);
+            textBox1.TabIndex = 0;
+            textBox1.KeyPress += textBox1_KeyPress;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(label11);
+            tabPage2.Controls.Add(label10);
+            tabPage2.Controls.Add(textBox5);
+            tabPage2.Controls.Add(label8);
+            tabPage2.Controls.Add(label9);
+            tabPage2.Controls.Add(textBox4);
+            tabPage2.Controls.Add(label7);
+            tabPage2.Controls.Add(label6);
+            tabPage2.Controls.Add(label5);
+            tabPage2.Controls.Add(textBox3);
+            tabPage2.Controls.Add(label4);
+            tabPage2.Controls.Add(button2);
+            tabPage2.Controls.Add(textBox2);
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(768, 393);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "Завдання 2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(159, 307);
+            label11.Name = "label11";
+            label11.Size = new Size(58, 20);
+            label11.TabIndex = 16;
+            label11.Text = "label11";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(26, 307);
+            label10.Name = "label10";
+            label10.Size = new Size(58, 20);
+            label10.TabIndex = 15;
+            label10.Text = "label10";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(465, 179);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(125, 27);
+            textBox5.TabIndex = 14;
+            textBox5.KeyPress += textBox7_KeyPress;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(64, 186);
+            label8.Name = "label8";
+            label8.Size = new Size(50, 20);
+            label8.TabIndex = 13;
+            label8.Text = "label8";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(441, 186);
+            label9.Name = "label9";
+            label9.Size = new Size(50, 20);
+            label9.TabIndex = 12;
+            label9.Text = "label9";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(92, 179);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 10;
+            textBox4.KeyPress += textBox4_KeyPress;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(135, 141);
+            label7.Name = "label7";
+            label7.Size = new Size(50, 20);
+            label7.TabIndex = 9;
+            label7.Text = "label7";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(375, 68);
+            label6.Name = "label6";
+            label6.Size = new Size(50, 20);
+            label6.TabIndex = 8;
+            label6.Text = "label6";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(0, 68);
+            label5.Name = "label5";
+            label5.Size = new Size(50, 20);
+            label5.TabIndex = 7;
+            label5.Text = "label5";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(401, 68);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(338, 54);
+            textBox3.TabIndex = 6;
+            textBox3.KeyPress += textBox3_KeyPress;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(92, 14);
+            label4.Name = "label4";
+            label4.Size = new Size(50, 20);
+            label4.TabIndex = 5;
+            label4.Text = "label4";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(331, 252);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(26, 68);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(338, 54);
+            textBox2.TabIndex = 3;
+            textBox2.KeyPress += textBox2_KeyPress;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "ЛБ4";
+            Load += Form1_Load;
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label1;
+        private Button button1;
+        private TextBox textBox1;
+        private TabPage tabPage2;
+        private Label label3;
+        private Label label2;
+        private TextBox textBox3;
+        private Label label4;
+        private Button button2;
+        private TextBox textBox2;
+        private TextBox textBox5;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox4;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label11;
+        private Label label10;
+    }
+}
---
+++ ЛБ4/4/Form1.cs
@@ -0,0 +1,193 @@
+namespace _4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Введіть довільну кількість чисел масиву розділених комою ' , ' (після останнього числа кома не потрібна) :";
+            label2.Text = "Результат: ";
+            label3.Text = " ";
+            button1.Text = "Обчислити";
+
+            label4.Text = "Введіть довільну кількість чисел для двовимірного масиву розділених комою ' , '\n (після останнього числа кома не потрібна) :";
+            label5.Text = "1.";
+            label6.Text = "2.";
+            label7.Text = "Введіть порядкові номери чисел з двохвимірного масиву: ";
+            label8.Text = "1.";
+            label9.Text = "2.";
+            label10.Text = "Результат: ";
+            label11.Text = " ";
+            button2.Text = "Обчислити";
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] elements = textBox1.Text.Split(',');
+            double[] array;
+            List<double> tempList = new List<double>();
+
+            foreach (var element in elements)
+            {
+                if (double.TryParse(element.Trim(), out double number))
+                {
+                    tempList.Add(number);
+                }
+                else
+                {
+                    MessageBox.Show("Зітріть кому після останнього числа!");
+                    return;
+                }
+            }
+
+            array = tempList.ToArray();
+
+            int maxIndex = FindMaxIndex(array);
+            label3.Text = $"Номер максимального елемента масиву: {maxIndex}\n";
+
+            double product = FindProductBetweenZeros(array);
+            label3.Text += $"Добуток елементів масиву між першим і другим нульовими елементами: {product}\n";
+
+            TransformArray(array);
+
+            label3.Text += "Масив після перетворення:\n";
+            foreach (var item in array)
+            {
+                label3.Text += item + " ";
+            }
+        }
+        static int FindMaxIndex(double[] array)
+        {
+            double max = array[0];
+            int maxIndex = 1;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] > max)
+                {
+                    max = array[i];
+                    maxIndex = i + 1;
+                }
+            }
+            return maxIndex;
+        }
+
+        static double FindProductBetweenZeros(double[] array)
+        {
+            int firstZeroIndex = Array.IndexOf(array, 0);
+            if (firstZeroIndex == -1)
+                return 0;
+
+            int secondZeroIndex = Array.IndexOf(array, 0, firstZeroIndex + 1);
+            if (secondZeroIndex == -1)
+                return 0;
+
+            double product = 1;
+            for (int i = firstZeroIndex + 1; i < secondZeroIndex; i++)
+            {
+                product *= array[i];
+            }
+            return product;
+        }
+
+        static void TransformArray(double[] array)
+        {
+            int halfLength = array.Length / 2;
+            for (int i = 0; i < halfLength; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    double temp = array[i];
+                    array[i] = array[halfLength + i];
+                    array[halfLength + i] = temp;
+                }
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != ',')
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox7_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[,] array = Create2DArray();
+
+            int row1 = 0; // Встановлено за умовою
+            int row2 = 1; // Встановлено за умовою
+            int column1 = int.Parse(textBox4.Text); // Введення користувача
+            int column2 = int.Parse(textBox5.Text); // Введення користувача
+
+            double difference = CalculateDifference(array, row1, column1, row2, column2);
+            double geometricMean = CalculateGeometricMean(array, row1, column1, row2, column2);
+
+            label11.Text = $"Різниця: {difference}\nСереднє геометричне: {geometricMean}";
+        }
+        private int[,] Create2DArray()
+        {
+            string[] row1Elements = textBox2.Text.Split(',');
+            string[] row2Elements = textBox3.Text.Split(',');
+
+            int maxLength = Math.Max(row1Elements.Length, row2Elements.Length);
+            int[,] array = new int[2, maxLength + 1]; // Збільшуємо розмір масиву на одиницю
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (i < row1Elements.Length)
+                    array[0, i + 1] = int.Parse(row1Elements[i]); // Починаємо з індексу 1
+
+                if (i < row2Elements.Length)
+                    array[1, i + 1] = int.Parse(row2Elements[i]); // Починаємо з індексу 1
+            }
+
+            return array;
+        }
+        private double CalculateDifference(int[,] array, int row1, int column1, int row2, int column2)
+        {
+            return Math.Abs(array[row1, column1] - array[row2, column2]);
+        }
+
+        private double CalculateGeometricMean(int[,] array, int row1, int column1, int row2, int column2)
+        {
+            double product = array[row1, column1] * array[row2, column2];
+            return Math.Sqrt(product);
+        }
+    }
+}
